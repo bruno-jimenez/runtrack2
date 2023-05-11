b2191a73918f2512d2bbe3309f12e9d923e2274f
unknown <bruno-jimenez@laplateforme.io>
2023-05-11 14:56:46 +0200

    adding day 2 and job 1

---
+++ jour 02/Job01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack2-J2job1</title>
+</head>
+<body>
+    <?php
+        for($x = 0; $x <= 1337; $x++){
+            if($x == 42){
+                echo "<b><u>".$x."</b></u><br>";
+            }
+            else{
+                echo $x. "<br>";
+            }
+        }
+    ?>
+</body>
+</html>
